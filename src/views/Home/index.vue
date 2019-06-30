102b6b308975f97454c310bc4a504c83677a340b
tflin <tflins@163.com>
2019-06-30 14:28:23 +0800

    完成home页面头像，name，签名

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="page home">
-    <h1>主页</h1>
+    <div class="avatar">
+      <img src="../../assets/avatar.jpg" alt="">
+    </div>
+    <h1 class="name">{{ name }}</h1>
+    <p class="shortDescription">{{ shortDescription }}</p>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      name: "谭丰林",
+      shortDescription: '前端新人，请多多指教'
+    }
   },
   components: {}
 }
@@ -15,6 +22,39 @@ export default {
   
 <style rel="stylesheet/scss" lang="scss" scoped>
 .home {
-  background-color: #368;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
+  color: aliceblue;
+
+  .avatar {
+    width: 150px;
+    height: 150px;
+    margin-top: -60px;
+
+    border-radius: 50%;
+    background-color: #fff;
+    border: 5px solid #fff;
+    box-shadow: 0 5px 10px rgba(0,0,0,.5);
+
+    overflow: hidden;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .name {
+    margin-top: 20px;
+    line-height: 60px;
+  }
+
+  .shortDescription {
+    // color: #777;
+    line-height: 60px;
+  }
 }
 </style>
