4d4adc35cd2612363ca77f4452ec71f1d8899474
tflin <tflins@163.com>
2019-12-16 12:56:05 +0800

    更新工作经历

--- src/views/WorkExperience/index.vue
+++ src/views/WorkExperience/index.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="page work-experience">
-    <h1 class="title">实习经历</h1>
+    <h1 class="title">工作经历</h1>
     <div class="work-body">
       <div class="item">
-        <h2 class="job">北京优炫软件 成都研发部 前端开发工程师</h2>
+        <h2 class="job">深圳威锋网 前端开发工程师</h2>
+        <p class="time">2019年7月 - 至今</p>
+        <p class="content">
+          负责重构威锋PC前端，独立完成项目架构的建设，新版威锋网采用 Nuxt.js 进行前端同构，正在开发中 预览地址 beta.feng.com
+        </p>
+        <p class="skills">
+          相关技术：Vue.js、Nuxt.js、前端同构、服务端渲染
+        </p>
+      </div>
+      <div class="item">
+        <h2 class="job">北京优炫软件 成都研发部 前端开发工程师（实习）</h2>
         <p class="time">2018年10月 - 2019年4月</p>
         <p class="content">
           参与开发了公司的 SIMO一体化运维系统，前期主要工作是修复公司项目 SIMO 一体化运维系统的缺陷，中期独立负责开发公司产品的移动端混合 APP 的前端页面及功能，
@@ -35,9 +45,13 @@ export default {
     color: #eee;
     display: flex;
     justify-content: center;
+    flex-direction: column;
+    align-items: center;
+
     .item {
       width: 65%;
       text-align: center;
+      margin-bottom: 50px;
 
       .time {
         opacity: 0.7;
