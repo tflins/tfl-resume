8dc3806f0dfcc38e69954751d82161c84df45b7b
tflin <tflins@163.com>
2020-02-11 23:11:36 +0800

    优化技能页面移动端体验

--- src/views/Skills/index.vue
+++ src/views/Skills/index.vue
@@ -7,7 +7,6 @@
         <div class="tag expert">Webpack</div>
       </div>
       <div class="tag-panel">
-        <div class="tag familiar">Bootstrap</div>
         <div class="tag familiar">jQuery</div>
         <div class="tag expert">Vue.js</div>
         <div class="tag expert">Git</div>
@@ -15,7 +14,7 @@
       </div>
       <div class="tag-panel">
         <div class="tag expert">HTML5</div>
-        <div class="tag expert">JavaScript(ES5/ES6+)</div>
+        <div class="tag expert">JavaScript</div>
         <div class="tag expert">Nuxt.js</div>
         <div class="tag expert">CSS</div>
       </div>
@@ -27,6 +26,34 @@
       <div class="tag-panel">
         <div class="tag familiar">PhotoShop</div>
         <div class="tag familiar">Mongodb</div>
+      </div>
+    </div>
+
+    <div class="skills-body-m">
+      <div class="tag-panel">
+        <div class="tag familiar">C语言</div>
+        <div class="tag familiar">Mongodb</div>
+        <div class="tag familiar">PhotoShop</div>
+      </div>
+      <div class="tag-panel">
+        <div class="tag expert">Vue.js</div>
+        <div class="tag expert">Git</div>
+        <div class="tag expert">Scss</div>
+      </div>
+      <div class="tag-panel">
+        <div class="tag expert">HTML5</div>
+        <div class="tag expert">JavaScript</div>
+        <div class="tag expert">Nuxt.js</div>
+      </div>
+      <div class="tag-panel">
+        <div class="tag expert">Node.js</div>
+        <div class="tag familiar">Koa2</div>
+        <div class="tag familiar">Express</div>
+      </div>
+      <div class="tag-panel">
+        <div class="tag familiar">jQuery</div>
+        <div class="tag expert">CSS</div>
+        <div class="tag expert">Webpack</div>
       </div>
     </div>
   </div>
@@ -44,6 +71,10 @@ export default {
 <style rel="stylesheet/scss" lang="scss" scoped>
 .skills {
   background-image: linear-gradient(to top, #0ba360 0%, #3cba92 100%);
+
+  .skills-body-m {
+    display: none;
+  }
 
   .skills-body {
     display: flex;
@@ -64,11 +95,12 @@ export default {
       margin: 20px 0;
 
       .tag {
+        min-width: 45px;
         color: #fff;
-        display: flex;
+        text-align: center;
         background-color: #096254;
         padding: 10px;
-        margin: 0 10px;
+        margin: 0 40px;
         border-radius: 4px;
       }
 
@@ -84,8 +116,50 @@ export default {
 }
 
 @media (max-width: 480px) {
-  .skills-body {
-    
+  .skills {
+    .skills-body {
+      display: none;
+    }
+    .skills-body-m {
+      display: block;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      margin-top: 40px;
+
+      .tag-panel {
+        display: flex;
+        flex-direction: row;
+        width: 60%;
+        text-align: center;
+        // align-items: center;
+        // justify-content: space-around;
+        justify-content: center;
+
+        margin: 20px 0;
+
+        .tag {
+          min-width: 66px;
+          font-size: 14px;
+          color: #fff;
+          text-align: center;
+          background-color: #096254;
+          padding: 10px;
+          margin: 0 5px;
+          border-radius: 4px;
+        }
+
+        .expert {
+          opacity: 1;
+        }
+
+        .familiar {
+          opacity: 0.7;
+        }
+      }
+    }
   }
 }
 </style>
