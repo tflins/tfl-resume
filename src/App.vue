63c59f5678605bf93adb87cbcb7ca71da520a797
tflin <tflins@163.com>
2020-12-13 22:38:14 +0800

    fix: 删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <template>
   <div id="app">
     <div class="t-full-page">
-      <div class="t-page">
-        <home class="home section"></home>
-      </div>
-      <div class="t-page">
-        <introductions class="section introductions"></introductions>
-      </div>
-      <div class="t-page">
-        <skills class="section skills"></skills>
-      </div>
-      <div class="t-page">
-        <work-experience class="section work-experience"></work-experience>
-      </div>
-      <div class="t-page">
-        <projects class="section projects"></projects>
-      </div>
-      <div class="t-page">
-        <more class="section more"></more>
-      </div>
+      <home class="home t-page"></home>
+      <introductions class="t-page introductions"></introductions>
+      <skills class="t-page skills"></skills>
+      <work-experience class="t-page work-experience"></work-experience>
+      <projects class="t-page projects"></projects>
+      <more class="t-page more"></more>
     </div>
   </div>
 </template>
@@ -37,28 +25,6 @@ import './lib/t-full-page.css'
 
 export default {
   name: 'app',
-
-  data() {
-    return {
-      options: {
-        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-        afterLoad: this.afterLoad,
-        scrollOverflow: true,
-        navigation: true,
-        sectionsColor: [
-          '#41b883',
-          '#ff5f45',
-          '#0798ec',
-          '#fec401',
-          '#1bcee6',
-          '#ee1a59',
-          '#2c3e4f',
-          '#ba5be9',
-          '#b4b8ab'
-        ]
-      }
-    }
-  },
 
   components: {
     Home,
